1165405cb0c089eb066daf00e9828ca59de895bf
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2011-04-08 15:54:53 -0400

    Cleaned up ticker to not need path.

--- ticker.sh
+++ ticker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd ~/scripts/gcal-ticker/
-./ticker.pl $@ | dzen2 -bg '#000000' -xs 2 -ta l -fn 6x12  2> /dev/null
+DZEN_OPTS=" -bg '#000000' -xs 2 -ta l -fn 6x12 "
+./ticker.pl $@ | dzen2 $DZEN_OPTS 2> /dev/null
